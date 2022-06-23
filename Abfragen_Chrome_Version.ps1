183356bf63fd06f727ce212fe9b1bf86d52a31ef
Jan Kraemer <45404328+sirditschonweg@users.noreply.github.com>
2022-06-23 13:36:12 +0200

    My First Script
    It is a Powershell script the reqest the Chorm Version from these PCs

---
+++ Abfragen_Chrome_Version.ps1
@@ -0,0 +1,12 @@
+﻿# Abfrage der Chrome Version
+# Invoke-Command -ComputerName desrvt0040 -Command {Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe'}
+
+ 
+$SName =@("PC-Name1" , "PC-Name2" , "PC-Name3", "PC-Name4")
+
+foreach ($SName in $SName)
+{
+Invoke-Command -ComputerName $SName -Command { 
+    (Get-Item (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe').'(Default)').VersionInfo
+                                              } | Select-Object -Property PSComputerName, ProductVersion  
+}
